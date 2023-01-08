6af0aaea15387bce7a856aac8bcab93ed40c6e21
Maxime <maxime.sciare@gmail.com>
2023-01-08 09:47:05 +0100

    update db

--- sql/conciergerie.sql
+++ sql/conciergerie.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : jeu. 05 jan. 2023 à 18:32
+-- Généré le : dim. 08 jan. 2023 à 09:45
 -- Version du serveur : 10.4.27-MariaDB
 -- Version de PHP : 8.1.12
 
@@ -38,7 +38,23 @@ CREATE TABLE `cadre_depense_points` (
 --
 
 INSERT INTO `cadre_depense_points` (`id_cadre_depense_points`, `id_historique_points`, `motif_utilisation`) VALUES
-(1, 1, 'cadeau offert');
+(1, 1, 'cadeau offert'),
+(2, 1, 'règle'),
+(3, 1, ''),
+(4, 1, ''),
+(5, 1, ''),
+(6, 1, ''),
+(7, 1, ''),
+(8, 1, ''),
+(9, 1, ''),
+(10, 1, 'kdo'),
+(11, 1, 'kdo'),
+(12, 1, 'kdo'),
+(13, 1, 'kdo'),
+(14, 1, ''),
+(15, 1, ''),
+(16, 1, 'kdo'),
+(17, 2, 'kdo');
 
 -- --------------------------------------------------------
 
@@ -57,7 +73,7 @@ CREATE TABLE `client` (
   `email` varchar(200) NOT NULL,
   `membership` varchar(15) NOT NULL,
   `is_ultimate` tinyint(4) NOT NULL,
-  `next_discount` tinyint(3) UNSIGNED NOT NULL,
+  `next_discount` int(10) UNSIGNED NOT NULL,
   `arrival_date` date NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -67,9 +83,9 @@ CREATE TABLE `client` (
 
 INSERT INTO `client` (`id_client`, `name`, `surname`, `code`, `postal_address`, `facebook_username`, `instagram_username`, `email`, `membership`, `is_ultimate`, `next_discount`, `arrival_date`) VALUES
 (6, 'Kévin', 'De la Gare', '22-SPR-0001', '6 Rue de la Pastaga', 'facebook', 'insta', 'mail@yahoo.fr', 'Silver', 0, 0, '2022-12-30'),
-(14, 'Jean Michel', 'de la gare', '22-SPR-0002', '6 Rue de la pastagaaaaaaaaaaaaa', 'fb', 'insta', 'mail@yahoo.fr', 'Silver', 0, 0, '2022-12-30'),
+(14, 'Jean Michel', 'de la gare', '22-SPR-0002', '6 Rue de la pastagaaaaaaaaaaaaa', 'fb', 'insta', 'mail@yahoo.fr', 'Gold', 0, 0, '2022-12-30'),
 (15, 'Michel', 'Dupont', '22-SPR-0003', 'Rue des Mimosas 75000 Paris', 'michou', 'dudu', 'dupont.michel@yahoo.fr', 'Silver', 0, 0, '2022-12-30'),
-(16, 'Jean', 'Némar', '23-SPR-0004', '17 Rue des Clébards', 'JN', 'JNN (Jean Neural Network)', 'jeannemar@gmail.com', 'Silver', 1, 0, '2023-01-05');
+(16, 'Jean', 'Némar', '23-SPR-0004', '17 Rue des Clébards', 'JN', 'JNN (Jean Neural Network)', 'jeannemar@gmail.com', 'Gold', 1, 0, '2023-01-05');
 
 -- --------------------------------------------------------
 
@@ -79,22 +95,17 @@ INSERT INTO `client` (`id_client`, `name`, `surname`, `code`, `postal_address`, 
 
 CREATE TABLE `commande` (
   `id_commande` bigint(20) UNSIGNED NOT NULL,
-  `id_points` bigint(20) UNSIGNED NOT NULL,
+  `id_points` bigint(20) UNSIGNED DEFAULT NULL,
   `numero` varchar(15) NOT NULL,
   `cmd_date` date NOT NULL,
   `cmd_arrival_date` date NOT NULL,
+  `cmd_dispatched_date` date NOT NULL,
   `status` varchar(15) NOT NULL,
   `delivery_price` int(11) NOT NULL,
   `service_price` int(11) NOT NULL,
+  `promotion_applied` int(10) UNSIGNED DEFAULT NULL,
   `note` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Déchargement des données de la table `commande`
---
-
-INSERT INTO `commande` (`id_commande`, `id_points`, `numero`, `cmd_date`, `cmd_arrival_date`, `status`, `delivery_price`, `service_price`, `note`) VALUES
-(14, 27, '050123-CMD-C001', '2023-01-05', '0000-00-00', 'to_buy', 0, 0, 'nonote');
 
 -- --------------------------------------------------------
 
@@ -106,7 +117,8 @@ CREATE TABLE `facture` (
   `id_facture` bigint(20) UNSIGNED NOT NULL,
   `id_commande` bigint(20) UNSIGNED NOT NULL,
   `facture_date` date NOT NULL,
-  `facture_date_maj` date NOT NULL
+  `facture_date_maj` date DEFAULT NULL,
+  `numero` varchar(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -121,13 +133,6 @@ CREATE TABLE `historique` (
   `quantity` int(10) NOT NULL,
   `sold_price` double NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Déchargement des données de la table `historique`
---
-
-INSERT INTO `historique` (`id_commande`, `id_produit`, `quantity`, `sold_price`) VALUES
-(14, 1, 1, 10);
 
 -- --------------------------------------------------------
 
@@ -145,6 +150,7 @@ CREATE TABLE `historique_points` (
 --
 
 INSERT INTO `historique_points` (`id_historique_points`, `id_client`) VALUES
+(2, 6),
 (1, 16);
 
 -- --------------------------------------------------------
@@ -158,13 +164,6 @@ CREATE TABLE `liste_client_commande` (
   `id_commande` bigint(20) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Déchargement des données de la table `liste_client_commande`
---
-
-INSERT INTO `liste_client_commande` (`id_client`, `id_commande`) VALUES
-(6, 14);
-
 -- --------------------------------------------------------
 
 --
@@ -175,13 +174,6 @@ CREATE TABLE `liste_paiement_commande` (
   `id_paiement` bigint(20) UNSIGNED NOT NULL,
   `id_commande` bigint(20) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Déchargement des données de la table `liste_paiement_commande`
---
-
-INSERT INTO `liste_paiement_commande` (`id_paiement`, `id_commande`) VALUES
-(21, 14);
 
 -- --------------------------------------------------------
 
@@ -200,7 +192,9 @@ CREATE TABLE `mode_paiement` (
 
 INSERT INTO `mode_paiement` (`id_mode_paiement`, `mode`) VALUES
 (6, 'carte'),
-(7, 'cheque');
+(7, 'cheque'),
+(8, 'liquide'),
+(9, 'points');
 
 -- --------------------------------------------------------
 
@@ -214,13 +208,6 @@ CREATE TABLE `paiement` (
   `montant` int(10) UNSIGNED NOT NULL,
   `payment_date` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Déchargement des données de la table `paiement`
---
-
-INSERT INTO `paiement` (`id_paiement`, `id_mode_paiement`, `montant`, `payment_date`) VALUES
-(21, 6, 5, '2023-01-05');
 
 -- --------------------------------------------------------
 
@@ -241,9 +228,23 @@ CREATE TABLE `points` (
 --
 
 INSERT INTO `points` (`id_points`, `id_client`, `nb_points`, `exp_date`, `id_cadre_depense_points`) VALUES
-(27, 16, 100, '2024-01-05', NULL),
 (28, 16, 150, '2024-01-05', 1),
-(29, 16, 75, '2023-01-15', NULL);
+(29, 16, 75, '2023-01-15', 3),
+(30, 16, 400, '2024-01-07', NULL),
+(32, 16, 50, '2023-01-31', 3),
+(33, 16, 75, '2023-01-31', 3),
+(34, 16, 20, '2023-01-31', 3),
+(35, 16, 150, '2024-01-07', 10),
+(36, 16, 10, '2023-01-31', 10),
+(37, 16, 10, '2023-01-31', 10),
+(38, 16, 10, '2023-01-31', 10),
+(39, 16, 10, '2023-01-31', 10),
+(40, 16, 10, '2023-01-31', 3),
+(41, 16, 10, '2023-01-31', 3),
+(42, 16, 150, '2024-01-07', NULL),
+(44, 6, 10, '2023-01-14', 17),
+(47, 14, 10, '2024-01-07', NULL),
+(55, 6, 15, '2023-01-15', NULL);
 
 -- --------------------------------------------------------
 
@@ -253,7 +254,7 @@ INSERT INTO `points` (`id_points`, `id_client`, `nb_points`, `exp_date`, `id_cad
 
 CREATE TABLE `produit` (
   `id_produit` bigint(20) UNSIGNED NOT NULL,
-  `product_name` varchar(50) NOT NULL,
+  `product_name` varchar(255) NOT NULL,
   `unit_price` double NOT NULL,
   `status` varchar(50) NOT NULL,
   `nb_dispo` int(10) UNSIGNED NOT NULL
@@ -264,8 +265,22 @@ CREATE TABLE `produit` (
 --
 
 INSERT INTO `produit` (`id_produit`, `product_name`, `unit_price`, `status`, `nb_dispo`) VALUES
-(1, 'Un bon produit', 10, 'stock', 2),
-(2, 'Extrait de roche de fou malade', 150, 'out_of_stock', 0);
+(3, 'Caudalie Duo Levre Main', 13, 'stock', 5),
+(4, 'Nuxe Lait Corps 200ml', 18, 'available', 0),
+(5, 'Nuxe set voyage', 29, 'not_available', 0),
+(6, 'Creme anti-ride Bio LA PROVENCALE', 49, 'out_of_stock', 0),
+(7, 'Creme de nuit anti-age bio NAE', 45, 'gift', 17),
+(8, 'Creme So bio ethic jour peaux mature argan', 49, 'packed', 1),
+(9, 'Serum nuit l&#039;huile de jouvence La provencale bio', 39, 'arrived', 0),
+(10, 'Soin contour yeux aloe vera so&#039;bio ethic Lea Nature', 39, 'dispatched', 1),
+(11, 'Creme nutritive nuit so&#039;bio ethic', 45, 'delivered', 0),
+(12, 'Soin des yeux anti-age irs bio LA PROVENCALE', 39, 'other', 1),
+(13, 'Soin yeux anti-age iris bio LA SOURCE', 45, 'stock', 6),
+(14, 'Creme anti-age nuit iris bio LA SOURCE', 45, 'available', 0),
+(15, 'Baume gel aloe vera so&#039;bio LEA NATURE', 39, 'not_available', 0),
+(16, 'Serum anti-age iris bio LA SOURCE', 45, 'out_of_stock', 0),
+(17, 'Miss Dior Blooming Bouquet EDT Roller Pearl 20ml', 650, 'stock', 2),
+(18, 'Dior J&#039;adore EDP Roller Pearl 20ml', 65, 'available', 0);
 
 -- --------------------------------------------------------
 
@@ -394,7 +409,7 @@ ALTER TABLE `telephone`
 -- AUTO_INCREMENT pour la table `cadre_depense_points`
 --
 ALTER TABLE `cadre_depense_points`
-  MODIFY `id_cadre_depense_points` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_cadre_depense_points` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT pour la table `client`
@@ -406,43 +421,43 @@ ALTER TABLE `client`
 -- AUTO_INCREMENT pour la table `commande`
 --
 ALTER TABLE `commande`
-  MODIFY `id_commande` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id_commande` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 
 --
 -- AUTO_INCREMENT pour la table `facture`
 --
 ALTER TABLE `facture`
-  MODIFY `id_facture` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id_facture` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT pour la table `historique_points`
 --
 ALTER TABLE `historique_points`
-  MODIFY `id_historique_points` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_historique_points` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT pour la table `mode_paiement`
 --
 ALTER TABLE `mode_paiement`
-  MODIFY `id_mode_paiement` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id_mode_paiement` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT pour la table `paiement`
 --
 ALTER TABLE `paiement`
-  MODIFY `id_paiement` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `id_paiement` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
 
 --
 -- AUTO_INCREMENT pour la table `points`
 --
 ALTER TABLE `points`
-  MODIFY `id_points` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+  MODIFY `id_points` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
 
 --
 -- AUTO_INCREMENT pour la table `produit`
 --
 ALTER TABLE `produit`
-  MODIFY `id_produit` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_produit` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT pour la table `telephone`
